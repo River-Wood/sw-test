f66713a4355df7cfa26a5d78b732ff6ae23a89a1
PERFICIENT\kinnon.wu <kinnon.wu@perficient.com>
2017-12-20 11:10:19 +0800

    add root

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open("v1").then(function(cache) {
       return cache.addAll([
+        "/sw-test/",
         "/sw-test/index.html",
         "/sw-test/style.css",
         "/sw-test/app.js",
